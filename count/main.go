b2417b78b59c24225247576847f4b436a466a831
Nikita Chechnev <chechnevnikita123@gmail.com>
2024-09-06 09:51:00 +0400

    Map feature unlocked

---
+++ count/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"arrays/datafile"
+	"fmt"
+	"log"
+)
+
+func main() {
+	lines, err := datafile.GetStrings("votes.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	counts := make(map[string]int)
+	for _, line := range lines {
+		counts[line]++
+	}
+	sort.counts(n)
+	for count, number := range counts {
+		fmt.Printf("%s: %d\n", count, number)
+	}
+}
